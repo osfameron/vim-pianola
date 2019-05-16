a1c1817d277399632a592b43491984957cccf970
Hakim.Cassimally <hakim.cassimally@bbc.co.uk>
2019-05-16 11:02:45 +0100

    Moving vim specifics into a Driver class
    In theory this should be easily repurposed for other editors
    (or for inside VimScript etc.)

--- diff.py
+++ diff.py
@@ -5,119 +5,138 @@ from subprocess import (Popen, call, PIPE)
 
 dmp = diff_match_patch.diff_match_patch()
 
-def send(text):
-    call(['vim', '--remote-send', text])
+class Vim:
+    def send(cls, text):
+        call(['vim', '--remote-send', text])
 
-def expr(text):
-    p = Popen(['vim', '--remote-expr', text], stdout=PIPE, stderr=PIPE)
-    (output, error) = p.communicate()
-    if error:
-        print("ERROR! " + error.decode("utf-8"))
-        return None
-    else:
-        return output.decode("utf-8")
+    def expr(cls, text):
+        p = Popen(['vim', '--remote-expr', text], stdout=PIPE, stderr=PIPE)
+        (output, error) = p.communicate()
+        if error:
+            print("ERROR! " + error.decode("utf-8"))
+            return None
+        else:
+            return output.decode("utf-8")
 
-def down(lines=1):
-    if lines:
-        send('%sj' % lines)
+    def down(cls, lines=1):
+        if lines:
+            cls.send('%sj' % lines)
 
-def right(count=1):
-    if count:
-        send('%s<Space>' % count)
+    def right(cls, count=1):
+        if count:
+            cls.send('%s<Space>' % count)
 
-def col(pos):
-    if pos:
-        send('%s|' % pos)
+    def col(cls, pos):
+        if pos:
+            cls.send('%s|' % pos)
 
-def type(text):
-    send('<ESC>i')
-    for char in text:
-        send(char)
-    send('<ESC>')
+    def type(cls, text):
+        cls.send('<ESC>i')
+        for char in text:
+            cls.send(char)
+        cls.send('<ESC>')
 
-def cr():
-    send("<Esc>:CR<CR>")
+    def cr(cls):
+        cls.send("<Esc>:CR<CR>")
 
-def line(pos):
-    ret = expr("line(%s)" % pos)
-    if ret == None:
-        return None
-    else:
-        return int(ret)
+    def line(cls, pos):
+        ret = cls.expr("line(%s)" % pos)
+        if ret == None:
+            return None
+        else:
+            return int(ret)
 
-def currentline():
-    return line("'.'")
+    def currentline(cls):
+        return cls.line("'.'")
 
-def getline(pos):
-    return expr("getline(%s)" % pos)
+    def getline(cls, pos):
+        return cls.expr("getline(%s)" % pos)
 
-def getcurrentline():
-    return getline("'.'")
+    def getcurrentline(cls):
+        return cls.getline("'.'")
 
-def path(text):
-    items = text.rstrip().split(" ")
-    filename = items[-1]
-    depth = len(items)-1
-    return (depth, filename)
+    def path(cls, text):
+        items = text.rstrip().split(" ")
+        filename = items[-1]
+        depth = len(items)-1
+        return (depth, filename)
 
-def expandedfolder(name, pos, depth):
-    if re.search(r'/$', name):
-        (d2, t2) = path(getline(pos+1))
-        if d2 == None:
-            return False
-        elif d2 > depth:
-            return True
+    def expandedfolder(cls, name, pos, depth):
+        if re.search(r'/$', name):
+            (d2, t2) = cls.path(getline(pos+1))
+            if d2 == None:
+                return False
+            elif d2 > depth:
+                return True
+            else:
+                return False
         else:
             return False
-    else:
-        return False
 
-def edit(name):
-    send('<Esc>:Explore<cr>')
-    send('<Esc><Down><Down><Esc>')
+    def edit(cls, name):
+        cls.send('<Esc>:Explore<cr>')
+        cls.send('<Esc><Down><Down><Esc>')
 
-    matchedPath = ""
-    descent = 1
+        matchedPath = ""
+        descent = 1
 
-    while(True):
-        text = getcurrentline()
-        pos = currentline()
-        (depth, currname) = path(text)
-        #print("CHECKING: pos=%s, depth=%s, currname=%s, descent=%s" % (pos, depth, currname, descent))
-        if depth == None:
-            raise Exception("End of list")
+        while(True):
+            text = getcurrentline()
+            pos = currentline()
+            (depth, currname) = cls.path(text)
+            #print("CHECKING: pos=%s, depth=%s, currname=%s, descent=%s" % (pos, depth, currname, descent))
+            if depth == None:
+                raise Exception("End of list")
 
-        elif depth < descent:
-            raise Exception("Not found")
+            elif depth < descent:
+                raise Exception("Not found")
 
-        if name == matchedPath + currname:
-            time.sleep(0.2)
-            cr()
-            return
+            if name == matchedPath + currname:
+                time.sleep(0.2)
+                cls.cr()
+                return
 
-        elif re.match(r'^' + matchedPath + currname, name):
-            matchedPath += currname
-            descent += 1
-            if not expandedfolder(currname, pos, depth):
-                cr()
+            elif re.match(r'^' + matchedPath + currname, name):
+                matchedPath += currname
+                descent += 1
+                if not expandedfolder(currname, pos, depth):
+                    cls.cr()
+            else:
+                if expandedfolder(currname, pos, depth):
+                    cls.cr()
+
+            cls.down()
+
+    def select(cls, d, r):
+        cls.send('<ESC>v')
+        if d:
+            cls.down(d)
+            cls.col(r)
         else:
-            if expandedfolder(currname, pos, depth):
-                cr()
+            cls.right(r-1)
 
-        down()
+    def delete(cls):
+        cls.send('x')
 
-# this command must be installed, because remote-send doesn't do mappings
-# and netrw maps <CR> to something quite mad, with no command to invoke it.
-# :command! CR :call feedkeys('<ESC>:<ESC><CR>')
-# edit("clojure-diff/README.md")
-# edit("test/foo/bar")
+    def start(cls):
+        cls.send('<ESC>:set paste<CR>')
+        cls.send('gg')
 
-def diff(b1, b2):
+    def end(cls):
+        cls.send('<ESC>:set nopaste<CR>')
+        cls.send(':<CR>')
+
+    # this command must be installed, because remote-send doesn't do mappings
+    # and netrw maps <CR> to something quite mad, with no command to invoke it.
+    # :command! CR :call feedkeys('<ESC>:<ESC><CR>')
+    # edit("clojure-diff/README.md")
+    # edit("test/foo/bar")
+
+def diff(driver, b1, b2):
     diffs = dmp.diff_main(b1, b2)
     dmp.diff_cleanupSemantic(diffs)
 
-    send('<ESC>:set paste<CR>')
-    send('gg')
+    driver.start()
 
     for (action, text) in diffs:
         print("DIFF: %s - '%s'" % (action, text))
@@ -128,31 +147,26 @@ def diff(b1, b2):
 
         if action == 0:
             if d:
-                down(d)
-                col(r+1)
+                driver.down(d)
+                driver.col(r+1)
             else:
-                right(r)
+                driver.right(r)
             time.sleep(0.2)
 
         elif action == 1:
-            type(text)
-            send('l')
+            driver.type(text)
+            driver.right()
 
         elif action == -1:
-            send('<ESC>v')
-            if d:
-                down(d)
-                col(r)
-            else:
-                right(r-1)
+            driver.select(d, r)
             time.sleep(0.5)
-            send('x')
+            driver.delete()
             time.sleep(0.1)
+
         else:
             raise Error("EEEEK!")
 
-    send('<ESC>:set nopaste<CR>')
-    send(':<CR>')
+    driver.end()
 
 jabber1 = '''Twas brillig, and the slithy toves
       Did gyre and gimble in the wabe:
@@ -163,7 +177,9 @@ jabber2 = '''Twas four o'clock and the lithe and slimy badger-lizards
 All flimsy and miserable were the shabby birds,
       And the lost green pigs bellowed.'''
 
-diff("", jabber1)
-diff(jabber1, jabber2)
-diff(jabber2, jabber1)
-diff(jabber1, "")
+driver = Vim()
+
+diff(driver, "", jabber1)
+diff(driver, jabber1, jabber2)
+diff(driver, jabber2, jabber1)
+diff(driver, jabber1, "")
